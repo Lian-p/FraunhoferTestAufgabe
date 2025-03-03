28f2316de8f7ed2ff3b4171ed1fc04e32610b076
Lian p. <98989126+Lian-p@users.noreply.github.com>
2025-03-03 18:46:28 +0100

    Update Main.java

--- src/main/java/de/Lian/Main.java
+++ src/main/java/de/Lian/Main.java
@@ -4,7 +4,7 @@ package de.Lian;
 public class Main {
     public static void main(String[] args) {
 
-        String filePath = "C:\\Users\\User\\Desktop\\destop\\Coding\\Code25\\FraunhoferTestAufgabe\\src\\main\\resources\\data\\numbers1.txt";
+        String filePath = "...\\src\\main\\resources\\data\\numbers1.txt";
 
         PalindromeCalculator palindromeCalculator = new PalindromeCalculator(filePath);
         System.out.println("Die lösung ist: " + palindromeCalculator.calculatePalindromeSum());
